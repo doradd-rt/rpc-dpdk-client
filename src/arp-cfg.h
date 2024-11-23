97cbc69c3707b0c3f9be628d6452c4c1acbe24e2
Scofield626 <scofield_zliu@outlook.com>
2024-11-23 18:28:17 +0000

    style: align arp_entries for clearer documentation

--- src/arp-cfg.h
+++ src/arp-cfg.h
@@ -3,6 +3,10 @@
  */
 
 const char *arp_entries[] = {
-    "DE:AD:BE:EF:7B:15", "b8:3f:d2:2a:e5:11", "b8:3f:d2:2a:e7:69",
-    "b8:ca:3a:6a:bc:58", "b8:ca:3a:6a:b9:20", "b8:ca:3a:69:cc:10",
+  "DE:AD:BE:EF:7B:15",
+  "b8:3f:d2:2a:e5:11",
+  "b8:3f:d2:2a:e7:69",
+  "b8:ca:3a:6a:bc:58",
+  "b8:ca:3a:6a:b9:20",
+  "b8:ca:3a:69:cc:10",
 };
